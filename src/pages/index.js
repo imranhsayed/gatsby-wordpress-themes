6556dba007424cc3b03a18dfbe007c527caeddfb
Imran Sayed <imranhsayed@gmail.com>
2020-05-03 16:49:36 +0530

    Add boilerplate

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,77 +1,10 @@
 import React from "react"
-import { graphql } from "gatsby";
 
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-
-const IndexPage = ( { data } ) => {
+const IndexPage = () => {
 
 	return (
-		<Layout data={ data.HWGraphQL }>
-			<SEO title="Home" header={ data.HWGraphQL.header }/>
-		</Layout>
+		<div>Hello World</div>
 	)
 };
-
-export const query = graphql`
-    query {
-        HWGraphQL {
-            header: getHeader {
-                siteLogoUrl
-                siteTagLine
-                siteTitle
-	            favicon
-            }
-            headerMenuItems: menuItems(where: {location: HCMS_MENU_HEADER}) {
-                edges {
-                    node {
-                        id
-                        menuItemId
-                        label
-                        url
-                        childItems {
-                            edges {
-                                node {
-                                    menuItemId
-                                    label
-                                    url
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            footer: getFooter {
-                copyrightText
-                sidebarOne
-                sidebarTwo
-                socialLinks {
-                    iconUrl
-                    iconName
-                }
-            }
-            footerMenuItems: menuItems(where: {location: HCMS_MENU_FOOTER}) {
-                edges {
-                    node {
-                        id
-                        menuItemId
-                        label
-                        url
-                        childItems {
-                            edges {
-                                node {
-                                    menuItemId
-                                    label
-                                    url
-                                    id
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-`;
 
 export default IndexPage
