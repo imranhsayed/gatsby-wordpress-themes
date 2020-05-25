a3ec3feaeeb23416fdb2cc8dd24d329c51cb0a9e
Imran Sayed <imranhsayed@gmail.com>
2020-05-25 23:09:54 +0530

    Add blog component

--- packages/gatsby-wordpress-theme-phoenix/src/templates/blog/index.js
+++ packages/gatsby-wordpress-theme-phoenix/src/templates/blog/index.js
@@ -2,38 +2,13 @@ import React from 'react';
 import Link from 'gatsby-link'
 
 import Layout from "../../components/layout";
-import Post from '../../components/blog-list/post';
-
-const NavLink = props => {
-	if ( ! props.test ) {
-		return <Link to={ props.url }>{ props.text }</Link>
-	} else {
-		return <span>{ props.text }</span>
-	}
-}
+import Blog from '../../components/blog-list/blog';
 
 const BlogTemplate = ( { pageContext } ) => {
 
-	const { group, index, first, last, pageCount } = pageContext
-	const previousUrl = index - 1 == 1 ? '/blog' : '/blog/' + ( index - 1 ).toString()
-	const nextUrl = '/blog/' + ( index + 1 ).toString();
-
 	return (
 		<Layout>
-			<h4>{ pageCount } Pages</h4>
-
-			{ group.map( ( { node } ) => (
-				<Post key={ node.id } post={ node } />
-			) ) }
-			<div className="previousLink">
-				<NavLink test={ first } url={ previousUrl } text="Go to Previous Page"/>
-			</div>
-			{ index < pageCount ? (
-				<div className="nextLink">
-					<NavLink test={ last } url={ nextUrl } text="Go to Next Page"/>
-				</div>
-			) : '' }
-
+			<Blog pageContext={ pageContext }/>
 		</Layout>
 	)
 }
