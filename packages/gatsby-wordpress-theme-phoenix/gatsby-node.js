6f605d3bad14e349d69b0953aadcce2d7aaa1391
Imran Sayed <imranhsayed@gmail.com>
2020-05-27 16:43:45 +0530

    Add zip

--- packages/gatsby-wordpress-theme-phoenix/gatsby-node.js
+++ packages/gatsby-wordpress-theme-phoenix/gatsby-node.js
@@ -3,6 +3,7 @@ const createAllPages = require( './create-pages/posts' );
 const createFrontPage = require( './create-pages/front-page' );
 const createBlogPage = require( './create-pages/blog' );
 const createArchivePage = require( './create-pages/archive' );
+const path = require( 'path' );
 
 // Create all pages.
 exports.createPages = async ( { actions, graphql } ) => {
@@ -11,4 +12,14 @@ exports.createPages = async ( { actions, graphql } ) => {
 	await createFrontPage( { actions, graphql } );
 	await createBlogPage( { actions, graphql } );
 	await createArchivePage( { actions, graphql } );
+};
+
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+	actions.setWebpackConfig({
+		resolve: {
+			alias: {
+				'~': path.resolve(__dirname, '../../node_modules')
+			}
+		},
+	})
 };
