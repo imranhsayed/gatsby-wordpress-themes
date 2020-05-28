ae7260e6b3c001708666622c41db09d77a814fc1
Mahvash Fatima <mahvashfatima@Mahvashs-MacBook-Air.local>
2020-05-28 23:34:24 +0530

    Add styles for search

--- packages/gatsby-wordpress-theme-phoenix/src/components/home/search/index.js
+++ packages/gatsby-wordpress-theme-phoenix/src/components/home/search/index.js
@@ -1,89 +1,80 @@
-import React from 'react';
-import config from '../../../../client-config';
-import { Link } from 'gatsby';
-import { isEmpty } from 'lodash';
-import './style.scss';
-import ClientSearch from "../client-search";
-import SearchBackgroundImg from '../../../images/home/search-background.png';
+import React from "react"
+import config from "../../../../client-config"
+import { Link } from "gatsby"
+import { isEmpty } from "lodash"
+import "./style.scss"
+import ClientSearch from "../client-search"
+import SearchBackgroundImg from "../../../images/home/search-background.png"
 
-const Search = ( props ) => {
-	const {
-		data: {
-			backgroundImage,
-			searchLabel,
-			taxonomies,
-			taxonomyIconOne,
-			taxonomyIconTwo,
-			taxonomyIconThree,
-		},
-		posts,
-		engine
-	} = props;
+const Search = props => {
+  const {
+    data: {
+      backgroundImage,
+      searchLabel,
+      taxonomies,
+      taxonomyIconOne,
+      taxonomyIconTwo,
+      taxonomyIconThree,
+    },
+    posts,
+    engine,
+  } = props
 
-	const taxonomyIcons = [ taxonomyIconOne, taxonomyIconTwo, taxonomyIconThree ];
+  const taxonomyIcons = [taxonomyIconOne, taxonomyIconTwo, taxonomyIconThree]
 
-	const backgroundURL = ! isEmpty( backgroundImage )
-		? backgroundImage.sourceUrl
-		: config.searchSection.searchBackURL;
-	const placeholderText = ! isEmpty ( searchLabel )
-		? searchLabel
-		: config.searchSection.searchPlaceholderTxt;
+  const backgroundURL = !isEmpty(backgroundImage)
+    ? backgroundImage.sourceUrl
+    : config.searchSection.searchBackURL
+  const placeholderText = !isEmpty(searchLabel)
+    ? searchLabel
+    : config.searchSection.searchPlaceholderTxt
 
-	return ! isEmpty( props.data ) ? (
-		<div
-			className="search-section"
-			style={ {
-				background: `url( ${ backgroundURL } )`,
-				backgroundPosition: "center",
-				backgroundSize: "cover",
-			} }
-		>
+  return !isEmpty(props.data) ? (
+    <div
+      className="search-section"
+      style={{
+        background: `url( ${backgroundURL} )`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      {/* Search */}
+      <ClientSearch
+        posts={posts}
+        engine={engine}
+        placeholder={placeholderText}
+      />
 
-			{ ! isEmpty( taxonomies ) ? (
-				<div className="search-section__categories">
-					{ taxonomies.map( ( term, index ) => (
-						<div
-							key={ term.uri }
-							className="search-section__category"
-						>
-							{ ! isEmpty( taxonomyIcons[index].sourceUrl ) ? (
-								<div className="search-section__category-icon">
-									<img
-										src={ taxonomyIcons[index].sourceUrl }
-										alt={ taxonomyIcons[index].altText }
-									/>
-								</div>
-							) : (
-								<div className="search-section__category-icon">
-									<img
-										src={
-											config.searchSection.images[ index ]
-										}
-										alt="category icon"
-									/>
-								</div>
-							) }
-							{ term.name ? (
-								<Link to={ term.uri }>
-									{ term.name }
-								</Link>
-							) : (
-								''
-							) }
-						</div>
-					) ) }
-				</div>
-			) : (
-				''
-			) }
+      {!isEmpty(taxonomies) ? (
+        <div className="search-section__categories">
+          {taxonomies.map((term, index) => (
+            <div key={term.uri} className="search-section__category">
+              {!isEmpty(taxonomyIcons[index].sourceUrl) ? (
+                <div className="search-section__category-icon">
+                  <img
+                    src={taxonomyIcons[index].sourceUrl}
+                    alt={taxonomyIcons[index].altText}
+                  />
+                </div>
+              ) : (
+                <div className="search-section__category-icon">
+                  <img
+                    src={config.searchSection.images[index]}
+                    alt="category icon"
+                  />
+                </div>
+              )}
+              {term.name ? <Link to={term.uri}>{term.name}</Link> : ""}
+            </div>
+          ))}
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  ) : (
+    ""
+  )
+}
 
-			{/* Search */}
-			<ClientSearch posts={ posts } engine={ engine } placeholder={ placeholderText }/>
-
-		</div>
-	) : (
-		''
-	);
-};
-
-export default Search;
+export default Search
